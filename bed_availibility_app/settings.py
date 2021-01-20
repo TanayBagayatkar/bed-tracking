980e34e9994a1aa3ef2268f96e1337d35964ff48
Ebrahim Gabajiwala <ebrahimgabajiwala@gmail.com>
2021-01-20 23:32:22 +0530

    Added psql backend

--- bed_availibility_app/settings.py
+++ bed_availibility_app/settings.py
@@ -74,9 +74,17 @@ WSGI_APPLICATION = 'bed_availibility_app.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'BedAvailibilityApp', 
+        'USER': 'postgres', 
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
